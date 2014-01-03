deea47fa54e93dac684506e48924962eb5986481
Gregor Aisch <contact@vis4.net>
2014-01-03 12:38:46 +0100

    Base skeleton for visualization code

---
+++ d3-bubble-chart/static/bubble-chart.js
@@ -0,0 +1,11 @@
+(function () {
+
+    dw.visualization.register('bubble-chart', {
+
+        render: function($element, dataset, axes, theme) {
+            // render the vis here...
+        }
+
+    });
+
+}).call(this);
