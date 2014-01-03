2a75fdb29ce4466ffab6043695721be675fcc44b
Gregor Aisch <contact@vis4.net>
2014-01-03 11:52:01 +0100

    define the vis meta and register the vis

--- d3-bubble-chart/plugin.php
+++ d3-bubble-chart/plugin.php
@@ -3,7 +3,22 @@
 class DatawrapperPlugin_D3BubbleChart extends DatawrapperPlugin {
 
     public function init(){
-        // do some stuff here
+        // define the visualization meta
+        $visMeta = array(
+            /*
+             * The unique visualization id (not to be confused with the plugin id)
+             */
+            "id" => "bubble-chart",
+
+            /*
+             * The title displayed in the editor UI. Wrap in __() to make it
+             * localizable.
+             */
+            "title" => "Bubble Chart (d3)"
+        );
+
+        // register the visualization
+        DatawrapperVisualization::register($this, $visMeta);
     }
 
 }
