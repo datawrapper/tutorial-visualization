0473c7f3ff6c05826729027930cb65d5f7369393
Gregor Aisch <contact@vis4.net>
2014-01-03 12:53:02 +0100

    preparing the data structure

--- d3-bubble-chart/static/bubble-chart.js
+++ d3-bubble-chart/static/bubble-chart.js
@@ -3,7 +3,19 @@
     dw.visualization.register('bubble-chart', {
 
         render: function($element, dataset, axes, theme) {
-            // render the vis here...
+            // create the empty structure
+            var data = { children: [] };
+            // loop over each row in our dataset
+            dataset.eachRow(function(i) {
+                // append new objects for each row
+                // with the values from the axes
+                data.children.push({
+                    label: axes.label.val(i),
+                    value: axes.size.val(i)
+                });
+            });
+            // let's see if this worked
+            console.log(data);
         }
 
     });
